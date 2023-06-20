0fb57ce4ba3d60aab01b5ac135bdc7da672425ae
iarba <dociarba@gmail.com>
2023-06-20 20:27:32 +0200

    updated install script to better fit my needs

--- install.sh
+++ install.sh
@@ -2,7 +2,9 @@
 
 echo "higher privilege might be require for a bunch of these commands"
 
+service supervisor stop
 cp catbot /opt/catbot/catbot
 chown catbot /opt/catbot/catbot
 chmod u+x /opt/catbot/catbot
 setcap CAP_NET_BIND_SERVICE=+eip /opt/catbot/catbot
+service supervisor start
